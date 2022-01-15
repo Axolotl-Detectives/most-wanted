de649bb6e3ad3b398a63f93de190cf7a701fc278
Andrew Talle <talle.andrew@gmail.com>
2022-01-15 11:38:52 -0700

    search-by-field office && sortBy most recent convicts

--- server/controllers/convictController.js
+++ server/controllers/convictController.js
@@ -4,14 +4,24 @@ const convictController = {};
 
 convictController.fetchMostWanted = (req, res, next) => {
   const base = 'https://api.fbi.gov/wanted/v1/list';
+  let query;
+  let URL;
 
-  console.log('called fetchMostWanted')
-  fetch(base, {
+  if (Object.keys(req.query).length > 0) {
+    query = `?field_offices=${req.query.field_offices}`
+    URL = base + query;
+  } else {
+    URL = base;
+  }
+  
+  fetch(URL, {
     method: "GET",
   })
   .then(response => response.json())
   .then(data => {
-    res.locals.convicts = data;
+    const sortedByPublication = data.items.sort(compare).slice(0, 20);
+
+    res.locals.convicts = sortedByPublication;
     return next();
   })
   .catch(err => {
@@ -22,6 +32,19 @@ convictController.fetchMostWanted = (req, res, next) => {
     });
   })
 
+
+  function compare(a, b) {
+    const dateA = new Date(a.publication);
+    const dateB = new Date(b.publication);
+
+    if ( dateA > dateB ){
+      return -1;
+    }
+    if ( dateA < dateB ){
+      return 1;
+    }
+    return 0;
+  }
 }
 
 module.exports = convictController;
