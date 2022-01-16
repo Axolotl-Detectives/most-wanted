88a7297fe4c895fa3245cf800a05974f2f8e590d
Andrew Talle <talle.andrew@gmail.com>
2022-01-15 20:23:52 -0700

    create async.js to fetch server data

--- client/components/Cards.js
+++ client/components/Cards.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 import defaultImg from '../images/Miller.jpg'
+import { getConvictsFromFBI, getConvictByFieldOffice } from '../async';
 
 function Cards() {
+  const [convicts, setConvicts] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    // examples of how to call our server.js from frontend
+    // getConvictsFromFBI().then(data => setConvicts(data));
+    getConvictByFieldOffice('dallas').then(data => setConvicts(data));
+
+    
+    setLoad(false);
+  }, [])
+
+  const renderCards = () => {
+    console.log(convicts);
+    return convicts.map(convict => {
+      console.log(convict);
+      return (
+        <CardItem
+          src={defaultImg}
+          text={convict?.title}
+          lable={convict?.title}
+          path='/profile'
+        />
+      )
+    })
+  }
+
   return (
     <div className='cards'>
       <h1>Have You Seen These Convicts</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src={defaultImg}
-              text='Card'
-              label='Profile'
-              path='/profile'
-            />
-            <CardItem
-              src={defaultImg}
-              text='Card'
-              label='Profile'
-              path='/profile'
-            />    
-          </ul>
+          {!load && renderCards()}
         </div>
       </div>
     </div>
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             MOST WANTED
-            <i class='fab fa-typo' />
+            <i className='fab fa-typo' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
